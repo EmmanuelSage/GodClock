83554358e53fd40dbb18fe4c63c86069935c6922
Emmanuel Oluyale <40043468+EmmanuelSage@users.noreply.github.com>
2021-07-08 05:34:14 +0100

    change 24 back to 30 for consistency

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import GodClock from "./components/GodClock/GodClock";
 
 function App() {
-  const [value, setValue] = useState(24)
+  const [value, setValue] = useState(30)
 
 const onClickHandler = (value: number) => {
   setValue(value);
